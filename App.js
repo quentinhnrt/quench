b0457fe826bf9ee2c3c82bd63597936017c214f0
Telmo Santos <telmo_escapaes@hotmail.com>
2022-10-13 18:32:44 +0100

    Card Component finished

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './card';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Card>
+        <Text>Open up App.js to start working on your app!</Text>
+      </Card>
       <StatusBar style="auto" />
     </View>
   );
