e5fa1780635b7ba9a41e8a0f17741d2fba3ef854
Quentin <qhonnart@amphibee.fr>
2022-11-22 13:47:44 +0100

    Add Card component

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
-import { NavigationContainer, useNavigationContainerRef, useRoute } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 import OnBoarding from './src/OnBoarding/OnBording';
 import Homepage from './src/Homepage/Homepage';
 import Header from './src/Header';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import BottomBar from './src/BottomBar';
+import CardDetails from "./src/Card/CardDetails";
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator tabBar={props => <BottomBar {...props} />} screenOptions={{
-        header: props => <Header {...props} />,
-      }}>
-        <Tab.Screen name="Home" component={Homepage} />
-        <Tab.Screen options={{ headerShown: false }} name="OnBoarding" component={OnBoarding} />
-        <Tab.Screen name="Profile" component={Homepage} />
-        <Tab.Screen name='StoreMap' component={Homepage} />
-        <Tab.Screen name='Camera' component={Homepage} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator tabBar={props => <BottomBar {...props} />} screenOptions={{
+                header: props => <Header {...props} />,
+            }}>
+                <Tab.Screen name="Home" component={Homepage}/>
+                <Tab.Screen options={{headerShown: false}} name="OnBoarding" component={OnBoarding}/>
+                <Tab.Screen name="Profile" component={Homepage}/>
+                <Tab.Screen name='StoreMap' component={Homepage}/>
+                <Tab.Screen name='Camera' component={Homepage}/>
+                <Tab.Screen name={'CardDetails'} component={CardDetails}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,10 +1,25 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, View,FlatList ,TouchableOpacity} from 'react-native'
+import React, { useState } from 'react'
 
-export default function Homepage(props) {
+import {globalStyles} from "../global";
+import Card from "../Card/Card";
+
+
+export default function Homepage({props,navigation}) {
+    const [reviews]= useState([
+        {title:'Starbucks', body: 'lorem ipsum' },
+        {title:'Teste', body: 'lorem ipsum' },
+    ]);
     return (
-      <View>
-        <Text>Homepage</Text>
-      </View>
-    )
+        <View>
+            <Text>Homepage</Text>
+            <FlatList data={reviews} renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => navigation.navigate('CardDetails', item)}>
+                    <Card>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </Card>
+                </TouchableOpacity>
+            )} />
+        </View>
+    );
 }
