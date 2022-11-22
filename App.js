563173024ee7bacebd88c960635b95a02cc474bd
Quentin Honnart <quentin.honnart@gmail.com>
2022-11-23 00:27:51 +0100

    change icons to npm library

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import * as React from 'react';
-import { NavigationContainer, useNavigationContainerRef, useRoute } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer, useNavigationContainerRef, useRoute} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import OnBoarding from './src/OnBoarding/OnBording';
 import Homepage from './src/Homepage/Homepage';
 import Header from './src/Header';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import BottomBar from './src/BottomBar';
 import Camera from "./src/Camera/Camera";
 
@@ -14,19 +14,19 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
 
-  return (
-
-    <NavigationContainer>
-      <Tab.Navigator tabBar={props => <BottomBar {...props} />} screenOptions={{
-        header: props => <Header {...props} />,
-      }}>
-        <Tab.Screen name="Home" component={Homepage} />
-        <Tab.Screen options={{ headerShown: false }} name="OnBoarding" component={OnBoarding} />
-        <Tab.Screen name="Profile" component={Homepage} />
-        <Tab.Screen name='StoreMap' component={Homepage} />
-        <Tab.Screen name='Camera' component={Camera} />
-      </Tab.Navigator>
-    </NavigationContainer>
-
-  );
+    return (
+        <SafeAreaView style={{width: '100%', height: "100%"}}>
+            <NavigationContainer>
+                <Tab.Navigator tabBar={props => <BottomBar {...props} />} screenOptions={{
+                    header: props => <Header {...props} />,
+                }}>
+                    <Tab.Screen name="Home" component={Homepage}/>
+                    <Tab.Screen options={{headerShown: false}} name="OnBoarding" component={OnBoarding}/>
+                    <Tab.Screen name="Profile" component={Homepage}/>
+                    <Tab.Screen name='StoreMap' component={Homepage}/>
+                    <Tab.Screen name='Camera' component={Camera}/>
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
+    );
 }
